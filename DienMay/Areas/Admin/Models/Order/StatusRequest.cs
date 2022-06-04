080b72dae394035d5169b90a15942619028278af
nghiado.dev <nghiadv1006@gmail.com>
2022-06-04 12:38:05 +0700

    first commit

---
+++ DienMay/Areas/Admin/Models/Order/StatusRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DienMay.Areas.Admin.Models.Order
+{
+    public class StatusRequest
+    {
+        public long Id { get; set; }
+        public int? PaymentStatus { get; set; }
+    }
+}
